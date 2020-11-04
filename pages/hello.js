5b537c6e5465f34aed68758a710417d48f0bdbb7
bossjain <bossjain59@yahoo.com>
2020-11-04 14:15:31 +0530

    Added Hello page

---
+++ pages/hello.js
@@ -0,0 +1,9 @@
+export default function Hello() {
+  return (
+    <div className="container">
+      <button type="button" className="btn btn-outline-success mt-4">
+        Hellos
+      </button>
+    </div>
+  );
+}
